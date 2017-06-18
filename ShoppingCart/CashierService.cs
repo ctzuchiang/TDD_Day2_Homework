23c9bfed3eff5d802658a1d2783d20c52634636e
joseph.chao <joseph.chao@titansoft.com.sg>
2017-06-18 20:13:41 +0800

    Initialize first test case :
    Purchase_HarryPotter_episode_1_price_is_100()

---
+++ ShoppingCart/CashierService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ShoppingCart
+{
+    public static class CashierService
+    {
+        public static int GetPurchasingPrice<TSource>(this IEnumerable<TSource> source, IEnumerable<TSource> product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class Product
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ ShoppingCartTest/CashierServiceTest.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ShoppingCart;
+
+namespace ShoppingCartTest
+{
+    [TestClass]
+    public class CashierServiceTest
+    {
+        [TestMethod]
+        public void Purchase_HarryPotter_episode_1_price_is_100()
+        {
+            IEnumerable<Product> products = new List<Product>()
+            {
+                new Product(){Name = "HarryPotter_1"}
+            };
+
+            var actual = products.GetPurchasingPrice(products);
+
+            Assert.Equals(actual, 100);
+        }
+
+        //[TestMethod]
+        //public void Purchase_HarryPotter_episode_1_price_is_100()
+        //{
+        //    //Arrange
+        //    //當我到了收銀台
+        //    var target = new CashierService();
+            
+        //    //Act
+        //    //給收銀台 哈利波特 第 1 集 1 本
+        //    var products = new List<Product>()
+        //    {
+        //        new Product(){Name = "HarryPotter_1"}
+        //    };
+
+        //    var actual = target.GetPurchasingPrice(products);
+            
+        //    //Assert
+        //    //結帳價格為 100 元
+        //    Assert.Equals(actual, 100);
+        //}
+    }
+}
