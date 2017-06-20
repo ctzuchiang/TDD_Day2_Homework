a962b1527227eb3de483f7a8a9c75474e7a9b17e
joseph.chao <joseph.chao@titansoft.com.sg>
2017-06-20 23:54:26 +0800

    Add Purchase_HarryPotter_episode1_1_and_episode2_1_price_is_190_after_5_percent_discount Test Case

--- ShoppingCartTest/CashierServiceTest.cs
+++ ShoppingCartTest/CashierServiceTest.cs
@@ -8,7 +8,7 @@ namespace ShoppingCartTest
     public class CashierServiceTest
     {
         [TestMethod]
-        public void Purchase_HarryPotter_episode_1_price_is_100()
+        public void Purchase_HarryPotter_episode1_1_price_is_100()
         {
             //Arrange
             //當我到了收銀台
@@ -26,6 +26,28 @@ namespace ShoppingCartTest
             //Assert
             //結帳價格為 100 元
             Assert.AreEqual(actual, 100);
+        }
+
+        [TestMethod]
+        public void Purchase_HarryPotter_episode1_1_and_episode2_1_price_is_190_after_5_percent_discount()
+        {
+            //Arrange
+            //當我到了收銀台
+            var target = new CashierService();
+
+            //Act
+            //給收銀台 哈利波特 第 1 集 1 本, 第 2 集 1 本
+            var products = new List<Product>()
+            {
+                new Product(){Name = "HarryPotter_1",Count = 1},
+                new Product(){Name = "HarryPotter_2",Count = 1}
+            };
+
+            var actual = target.GetPurchasingPrice(products);
+
+            //Assert
+            //結帳價格為 100 元
+            Assert.AreEqual(actual, 190);
         }
     }
 }
