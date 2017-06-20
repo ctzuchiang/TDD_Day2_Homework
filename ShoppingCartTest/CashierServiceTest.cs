c6683ecac6ba97509949e312a23dd626018f90ba
joseph.chao <joseph.chao@titansoft.com.sg>
2017-06-21 01:42:44 +0800

    Add Test Case Purchase_HarryPotter_episode_from_1_to_4_price_is_320_after_20_percent_discount()

--- ShoppingCartTest/CashierServiceTest.cs
+++ ShoppingCartTest/CashierServiceTest.cs
@@ -55,7 +55,6 @@ namespace ShoppingCartTest
         public void Purchase_HarryPotter_episode_from_1_to_3_price_is_270_after_10_percent_discount()
         {
             //Arrange
-            //當我到了收銀台
             var products = new List<Product>()
             {
                 new Product(){Name = "HarryPotter",Episode = 1,Count = 1},
@@ -66,13 +65,31 @@ namespace ShoppingCartTest
             var target = new CashierService();
 
             //Act
-            //給收銀台 哈利波特 第 1 集 1 本, 第 2 集 1 本, 第 3 集 1 本 
-
             var actual = target.GetPurchasingPrice(products);
 
             //Assert
-            //結帳價格為 270 元
             Assert.AreEqual(270, actual);
+        }
+
+        [TestMethod]
+        public void Purchase_HarryPotter_episode_from_1_to_4_price_is_320_after_20_percent_discount()
+        {
+            //Arrange
+            var products = new List<Product>()
+            {
+                new Product(){Name = "HarryPotter",Episode = 1,Count = 1},
+                new Product(){Name = "HarryPotter",Episode = 2,Count = 1},
+                new Product(){Name = "HarryPotter",Episode = 3,Count = 1},
+                new Product(){Name = "HarryPotter",Episode = 4,Count = 1},
+            };
+
+            var target = new CashierService();
+
+            //Act
+            var actual = target.GetPurchasingPrice(products);
+
+            //Assert
+            Assert.AreEqual(320, actual);
         }
     }
 }
