25a52df458aace4443070e7f2ef93552f5a0bd6d
joseph.chao <joseph.chao@titansoft.com.sg>
2017-06-21 01:47:25 +0800

    add Test Case Purchase_HarryPotter_episode1_1_and_episode2_1_and_episode3_2_price_is_370()

--- ShoppingCartTest/CashierServiceTest.cs
+++ ShoppingCartTest/CashierServiceTest.cs
@@ -113,5 +113,25 @@ namespace ShoppingCartTest
             //Assert
             Assert.AreEqual(375, actual);
         }
+
+        [TestMethod]
+        public void Purchase_HarryPotter_episode1_1_and_episode2_1_and_episode3_2_price_is_370()
+        {
+            //Arrange
+            var products = new List<Product>()
+            {
+                new Product(){Name = "HarryPotter",Episode = 1,Count = 1},
+                new Product(){Name = "HarryPotter",Episode = 2,Count = 1},
+                new Product(){Name = "HarryPotter",Episode = 3,Count = 2},
+            };
+
+            var target = new CashierService();
+
+            //Act
+            var actual = target.GetPurchasingPrice(products);
+
+            //Assert
+            Assert.AreEqual(370, actual);
+        }
     }
 }
